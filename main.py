4d09919fe3f9e9a80b68bf4279b6b410a1801887
katsumi <k.m.penkoka@gmail.com>
2021-02-23 21:43:27 +0900

    #9 ✨ログ機能を追加

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import sys
 import argparse
+import json
+import logging
+import logging.handlers
 
-# 独自モジュール
-from config.application_environment import ApplicationEnvironment
+# ロガーを設定 log_levelについては下記を参照
+# NOTE: https://docs.python.org/ja/3/library/logging.html#levels
+def setup_logger(app_env, modname=__name__):
+    logger = logging.getLogger(modname)
+    logger.setLevel(app_env.LOG_LEVEL)
 
+    # コンソールに出力
+    sh = logging.StreamHandler()
+    sh.setLevel(app_env.LOG_LEVEL)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    # ファイルに出力 10MB 5ファイル分
+    fh = logging.handlers.RotatingFileHandler(app_env.LOG_FILE_PATH, maxBytes=1024 * 1024 * 10 , backupCount=5, encoding='utf-8')
+    fh.setLevel(app_env.LOG_LEVEL)
+    fh_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s')
+    fh.setFormatter(fh_formatter)
+    logger.addHandler(fh)
+    return logger
+
+
+# アプリの環境値を保管するクラス
+class ApplicationEnvironment:
+    def __init__(self, args_env):
+        json_file = open(f'config/env/{args_env}.json', 'r')
+        app_env = json.load(json_file)
+        self.LOG_LEVEL = app_env['log']['level']
+        self.LOG_FILE_PATH = app_env['log']['file_path']
+
+# Main関数
 if __name__ == '__main__':
     # コマンドライン引数を検査
     parser = argparse.ArgumentParser()
@@ -12,3 +43,8 @@ if __name__ == '__main__':
     
     # アプリの設定値を読み込み
     app_env = ApplicationEnvironment(args.env)
+
+    # ロガーの初期化
+    logger = setup_logger(app_env)
+    logging.debug('アプリの起動')
+    logger.debug('アプリの初期化に成功しました')
