41a8e9b72ee9ab8031935a9587aefcbfe4e84a1e
katsumi <k.m.penkoka@gmail.com>
2021-02-23 16:09:23 +0900

    #1 🎉改行コードを修正

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 sudo apt update
+
 sudo apt upgrade -y
+
 sudo apt install libatlas3-base
+
 pip3 install owlifttypeh-1.0.0-py3-none-linux_armv7l.whl
+
 sudo apt install libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0
+
 ./samples/setup.sh
