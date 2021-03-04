e2d65c54fd709ad2daab3f5ec336251fe23d7543
katsumi <k.m.penkoka@gmail.com>
2021-03-04 14:53:01 +0900

    #34 🚧

--- bluetooth_le.py
+++ bluetooth_le.py
@@ -1,61 +1,48 @@
+  
 from pybleno import *
+import sys
+import signal
+from EchoCharacteristic import *
+
+print('bleno - echo');
 
 bleno = Bleno()
 
-APPROACH_SERVICE_UUID = '13A28130-8883-49A8-8BDB-42BC1A7107F4'
-APPROACH_CHARACTERISTIC_UUID = 'A2935077-201F-44EB-82E8-10CC02AD8CE1'
-
-
-class ApproachCharacteristic(Characteristic):
-
-    def __init__(self):
-        Characteristic.__init__(self, {
-            'uuid': APPROACH_CHARACTERISTIC_UUID,
-            'properties': ['read', 'notify'],
-            'value': None
-        })
-
-        self._value = 0
-        self._updateValueCallback = None
-
-    def onReadRequest(self, offset, callback):
-        print()
-        callback(Characteristic.RESULT_SUCCESS, self._value)
-
-    def onSubscribe(self, maxValueSize, updateValueCallback):
-        print('ApproachCharacteristic - onSubscribe')
-
-        self._updateValueCallback = updateValueCallback
-
-    def onUnsubscribe(self):
-        print('ApproachCharacteristic - onUnsubscribe')
-
-        self._updateValueCallback = None
-
-
 def onStateChange(state):
-    print('on -> stateChange: ' + state)
+   print('on -> stateChange: ' + state);
 
-    if (state == 'poweredOn'):
-        bleno.startAdvertising('Approach', [APPROACH_SERVICE_UUID])
-    else:
-        bleno.stopAdvertising()
-
+   if (state == 'poweredOn'):
+     bleno.startAdvertising('echo', ['ec00'])
+   else:
+     bleno.stopAdvertising();
 
 bleno.on('stateChange', onStateChange)
-
-approachCharacteristic = ApproachCharacteristic()
-
-
+    
 def onAdvertisingStart(error):
-    print('on -> advertisingStart: ' + ('error ' + error if error else 'success'))
+    print('on -> advertisingStart: ' + ('error ' + error if error else 'success'));
 
     if not error:
         bleno.setServices([
             BlenoPrimaryService({
-                'uuid': APPROACH_SERVICE_UUID,
-                'characteristics': [
-                    approachCharacteristic
-                ]
+                'uuid': 'ec00',
+                'characteristics': [ 
+                    EchoCharacteristic('ec0F')
+                    ]
             })
         ])
+bleno.on('advertisingStart', onAdvertisingStart)
+
+bleno.start()
+
+print ('Hit <ENTER> to disconnect')
+
+if (sys.version_info > (3, 0)):
+    input()
+else:
+    raw_input()
+
+bleno.stopAdvertising()
+bleno.disconnect()
+
+print ('terminated.')
+sys.exit(1)
