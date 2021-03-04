81ae2cf3a9a52362b4a639ff40627116f6a6bb3f
katsumi <k.m.penkoka@gmail.com>
2021-03-04 11:36:45 +0900

    #34 ✨Bluetoothモジュールのインストールを追加

--- setup.sh
+++ setup.sh
@@ -12,6 +12,9 @@ pip3 install owlifttypeh-1.0.0-py3-none-linux_armv7l.whl
 
 sudo apt install -y libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0
 
+# bluetooth
+sudo apt install -y bluetooth libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev libdbus-glib-1-dev libbluetooth-dev
+
 sudo chmod 777 samples/*
 
 # install dependency
@@ -21,3 +24,16 @@ pip3 install "Kivy>=1.11,<2.0"
 pip3 install "Pillow"
 pip3 install "requests"
 pip3 install "schedule"
+
+# bluetooth (python)
+cd ..
+wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.56.tar.xz
+xz -dv bluez-5.56.tar.xz
+tar -xf bluez-5.56.tar
+cd bluez-5.56/
+./configure --enable-experimental
+make -j4
+sudo make install
+sudo pip3 install pybluez
+cd ..
+cd taskal-timecard-thermometer
