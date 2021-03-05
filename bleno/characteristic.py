8b699cf2493f60e606eb50913c4ad9dcaecee9a5
katsumi <k.m.penkoka@gmail.com>
2021-03-05 17:08:59 +0900

    #34 🚧

--- bleno/characteristic.py
+++ bleno/characteristic.py
@@ -9,9 +9,11 @@ class BodyTempCharacteristic(Characteristic):
     def __init__(self, uuid):
         Characteristic.__init__(self, {
             'uuid': uuid,
-            'properties': ['notify'],
+            'properties': ['read', 'write', 'notify'],
             'value': None
           })
+
+          
           
         self._value = array.array('B', [0] * 0)
         self._updateValueCallback = None
@@ -25,6 +27,24 @@ class BodyTempCharacteristic(Characteristic):
         print('onUnsubscribe');
         
         self._updateValueCallback = None
+
+    def onReadRequest(self, offset, callback):
+        print('EchoCharacteristic - %s - onReadRequest: value = %s' % (self['uuid'], [hex(c) for c in self._value]))
+        callback(Characteristic.RESULT_SUCCESS, self._value[offset:])
+
+    def onWriteRequest(self, data, offset, withoutResponse, callback):
+        self._value = data
+
+        print(type(data))
+
+        print('EchoCharacteristic - %s - onWriteRequest: value = %s' % (self['uuid'], [hex(c) for c in self._value]))
+
+        if self._updateValueCallback:
+            print('EchoCharacteristic - onWriteRequest: notifying');
+            
+            self._updateValueCallback(self._value)
+        
+        callback(Characteristic.RESULT_SUCCESS)
 
     # 体温計が温度を取得した際はここが更新される
     def updateBodyTemp(self, measured_temperature, measured_distance):
