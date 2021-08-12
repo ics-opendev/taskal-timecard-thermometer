d2bf04735e580533075080a2c0d79d6a9bd4c9b2
katsumi <k.m.penkoka@gmail.com>
2021-08-12 15:39:19 +0900

    #45 ✨測定結果のクラスを作成

---
+++ entity/body_surface_temperature.py
@@ -0,0 +1,15 @@
+# coding: UTF-8
+from entity.enum import MeasurementType
+
+# 測定結果クラス
+class BodySurfaceTemperature:
+    # measurement_typeがNO_MEASUREMENTの場合はtemperatureが-1で固定される
+    def __init__(self, measurement_type, temperature):
+        # 測定方式
+        self.measurement_type = measurement_type
+        
+        # 測定温度
+        if measurement_type == MeasurementType.NO_MEASUREMENT:
+            self.temperature = -1
+        else:
+            self.temperature = temperature
