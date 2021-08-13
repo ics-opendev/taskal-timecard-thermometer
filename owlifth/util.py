036123519b6af9adeb8827ede01e2ce73579d48a
katsumi <k.m.penkoka@gmail.com>
2021-08-13 20:02:00 +0900

    #45 🐛

--- owlifth/util.py
+++ owlifth/util.py
@@ -6,13 +6,13 @@ def get_event_type(meta):
         return OwhMeta.EV_NONE
     
     event_type = meta.event_type
-    if (evt & OwhMeta.EV_BODY_TEMP) != 0:
+    if (event_type & OwhMeta.EV_BODY_TEMP) != 0:
         return OwhMeta.EV_BODY_TEMP
-    elif (evt & OwhMeta.EV_CORRECT) != 0:
+    elif (event_type & OwhMeta.EV_CORRECT) != 0:
         return OwhMeta.EV_CORRECT
-    elif (evt & OwhMeta.EV_DIST_INVALID) != 0:
+    elif (event_type & OwhMeta.EV_DIST_INVALID) != 0:
         return OwhMeta.EV_DIST_INVALID
-    elif (evt & OwhMeta.EV_LOST) != 0:
+    elif (event_type & OwhMeta.EV_LOST) != 0:
         return OwhMeta.EV_LOST
     else:
         return OwhMeta.EV_NONE
