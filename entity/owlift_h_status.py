d430e739770bba393c69d677888a0ab412f6757d
katsumi <k.m.penkoka@gmail.com>
2021-08-17 09:39:28 +0900

    #48 🚧

--- entity/owlift_h_status.py
+++ entity/owlift_h_status.py
@@ -3,6 +3,6 @@ from entity.enum.owlift_h_device_status import OwliftHDeviceStatus
 
 # owlift_hのステータスを記録したクラス
 class OwliftHStatus:
-    def __init__(self, preparation, status):
+    def __init__(self, status):
         # 現在のデバイスステータス
         self.status = status
