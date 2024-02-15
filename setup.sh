4a3ba88053c1703182484283346952d6a19980d9
katsumi <k.m.penkoka@gmail.com>
2024-02-15 12:16:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ set -e
 
 # うまく動かない時
 # https://kaworu.jpn.org/ubuntu/apt_update%E3%81%8C%E3%82%A4%E3%83%B3%E3%83%87%E3%83%83%E3%82%AF%E3%82%B9%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%81%AE%E3%83%80%E3%82%A6%E3%83%B3%E3%83%AD%E3%83%BC%E3%83%89%E3%81%AB%E5%A4%B1%E6%95%97%E3%81%99%E3%82%8B%E5%A0%B4%E5%90%88%E3%81%AE%E5%AF%BE%E5%87%A6%E6%B3%95
+sudo apt-get update
+sudo apt-get install
 sudo apt update
 
 sudo apt upgrade -y
